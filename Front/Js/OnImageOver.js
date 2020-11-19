4000071f9c27a97fb5ad63d4837d50765d1093cb
professoralex13 <alex@densem.net>
2020-11-19 16:20:02 +1300

    Added stuff

--- Front/Js/OnImageOver.js
+++ Front/Js/OnImageOver.js
@@ -1,9 +1,9 @@
 var images = document.getElementsByClassName("RobotOrAward");
 for (var i = 0; i < images.length; i++) {
-  images[i].addEventListener("mouseover", function (e) {
-    document.getElementById(event.target.id + "Text").style.opacity = "1";
-  });
-  images[i].addEventListener("mouseleave", function (e) {
-    document.getElementById(event.target.id + "Text").style.opacity = "0.7";
-  });
+    images[i].addEventListener("mouseover", function(e) {
+        document.getElementById(event.target.id + "Text").style.opacity = "1";
+    });
+    images[i].addEventListener("mouseleave", function(e) {
+        document.getElementById(event.target.id + "Text").style.opacity = "0.7";
+    });
 }
